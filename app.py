f5ed7ba158c528d47d36846567be0305d713632e
Daan Duppen <duppen.io@gmail.com>
2021-08-05 13:51:59 +0200

    feat: Date range for graph

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 import pydeck as pdk
 from pydeck.types import String
 
-# streamlit run first_app.py
+# streamlit run app.py
+# https://share.streamlit.io/daanduppen/sensor-data-demo-app/main/app.py
 
 # Done
 # text alignment
@@ -21,69 +22,95 @@ from pydeck.types import String
 # tooltip for each dot
 
 
+def get_midpoint():
+    return {"lat": 52.3671182, "lon": 4.9038155}
+
+
 def get_sensor_locations():
-    sensor_locations = pd.read_csv('data/sensor_locations.csv')
+    sensor_locations = pd.read_csv("data/sensor_locations.csv")
     sensor_locations = sensor_locations.round(6)
     return sensor_locations
 
 
+def get_sensor_data(sensor_name):
+    sensor_data = pd.read_csv(f"data/vorigemaand_clean_sensor_{sensor_name}.csv", index_col=0)
+    sensor_data.date = pd.to_datetime(sensor_data.date, format="%Y%m%d %H")
+    return sensor_data
 
 
-def generate_sensor_map(midpoint, sensor_locations):
+def get_pydeck_chart(midpoint, sensor_locations):
     point_layer = (
-    pdk.Layer(
-        "ScatterplotLayer",
-        data=sensor_locations,
-        get_position="[lon, lat]",
-        get_radius=0.5,
-    ),
-)
+        pdk.Layer(
+            "ScatterplotLayer",
+            data=sensor_locations,
+            get_position="[lon, lat]",
+            get_radius=0.5,
+        ),
+    )
 
     text_layer = pdk.Layer(
-    "TextLayer",
-    sensor_locations,
-    pickable=True,
-    get_position="[lon, lat]",
-    get_text="name",
-    get_size=16,
-    get_color=[0, 0, 0],
-    get_angle=0,
-    get_text_anchor=String("middle"),
-    # can be center, bottom, top
-    get_alignment_baseline=String("bottom"),
-)
+        "TextLayer",
+        data=sensor_locations,
+        pickable=True,
+        get_position="[lon, lat]",
+        get_text="name",
+        get_size=16,
+        get_color=[0, 0, 0],
+        get_angle=0,
+        get_text_anchor=String("middle"),
+        # can be center, bottom, top
+        get_alignment_baseline=String("bottom"),
+    )
 
     initial_view_state = pdk.ViewState(
-            latitude=midpoint["lat"],
-            longitude=midpoint["lon"],
-            zoom=18,
-            pitch=50,
-        )
+        latitude=midpoint["lat"],
+        longitude=midpoint["lon"],
+        zoom=18,
+        pitch=50,
+    )
 
-    st.pydeck_chart(
-    pdk.Deck(
+    sensor_pydeck_chart = pdk.Deck(
         map_style="mapbox://styles/mapbox/light-v9",
         initial_view_state=initial_view_state,
         layers=[point_layer, text_layer],
     )
-)
+    return sensor_pydeck_chart
 
 
+def get_sensor_dropdown(sensor_locations):
+    selection_options = pd.Series(["all"]).append(sensor_locations["name"])
+    return selection_options
+
+sensor_name = '1-1'
 
 # data
-midpoint = {"lat": 52.3671182, "lon": 4.9038155}
+
+midpoint = get_midpoint()
 sensor_locations = get_sensor_locations()
-selection_options = pd.Series(['all']).append(sensor_locations['name'])
-sensor_data = pd.read_csv('data/vorigemaand_clean.csv', index_col=0)
+sensor_data = get_sensor_data(sensor_name)
+
+# components
+sensor_pydeck_chart = get_pydeck_chart(midpoint, sensor_locations)
+selection_options = get_sensor_dropdown(sensor_locations)
 
 # page
 st.title("Sensor dashboard")
 # sensor_locations
-
-generate_sensor_map(midpoint, sensor_locations)
+st.pydeck_chart(sensor_pydeck_chart)
 selection = st.selectbox("Choose point ID", selection_options)
 
-# sensor_data.loc[:,['date', 'T']]
-df = sensor_data.set_index('date')
-df = df.iloc[0:25]
-st.line_chart(df['T'])
+
+# sensor_data.loc[:,['index', 'T']]
+d = {'hour': sensor_data.date.dt.hour, 'week': sensor_data.date.dt.week}
+# df.groupby([df.date.dt.month, df.date.dt.day]).temperature.mean().to_frame()
+# sensor_data = sensor_data.groupby([sensor_data.date.dt.hour]).temperature.mean().to_frame()
+# df.groupby([df.date.dt.day]).temperature.mean().to_frame()
+# df.groupby([df.date.dt.hour]).temperature.mean().to_frame()
+
+'sensor 1-1'
+start_date = st.date_input('start_date', value=sensor_data.date.min(), min_value=sensor_data.date.min(), max_value=sensor_data.date.max())
+end_date = st.date_input('end_date', value=sensor_data.date.max(), min_value=sensor_data.date.min(), max_value=sensor_data.date.max())
+sensor_data = sensor_data.set_index("date")
+# sensor_data = sensor_data.iloc[0:25]
+selected_date_range = sensor_data["temperature"].loc[start_date: end_date]
+st.line_chart(selected_date_range)
--- src/clean_fake_data.py
+++ src/clean_fake_data.py
@@ -1,67 +1,84 @@
-
-
-import math
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def generate_x(hour_iterator):
-    x = [i for i in hour_iterator]
-    return x
 
 def show_scatter_plot(x, y):
     plt.plot(x, y)
-    plt.scatter(x, y,  alpha=0.5)
+    plt.scatter(x, y, alpha=0.5)
     plt.show()
 
-def generate_y(hour_iterator):
-    base_temp = 21
-    temp_amplitude = 5
-
-    b = 0.024
-
-    period = 2 * math.pi / b
-    print(period)
-
-    y = []
-    # for hour in hour_iterator:
-    for minute in hour_iterator:
-        temp = temp_amplitude * math.sin(minute/ period ) + base_temp
-        y.append(temp)
-    return y
-
-# def generate_y_new(hour_iterator):
-#     for hour in hour_iterator:
-
-
-if __name__ == '__main__':
-    # hour_iterator = range(0, 24)
-    # y = generate_y(hour_iterator)
-    # x = generate_x(hour_iterator)
-    # show_scatter_plot(x, y)
-
-    df = pd.read_csv('vorigemaand.csv')
+def strip_column_names(df):
     df.columns = df.columns.str.strip()
-    cols_to_keep = ['YYYYMMDD', 'HH', 'T', '# STN']
-    df = df.loc[:, cols_to_keep]
-    df = df.rename(columns={'YYYYMMDD':'date', '# STN':'station_number'})
-    df = df.loc[14914:15657]
-    df = df.loc[df['station_number'] == 279]
-    df = df.drop(columns=['station_number']).reset_index()
-    df['T'] = pd.to_numeric(df['T'])
-    df['T'] = df['T'] / 10
-    df['HH'] = df['HH'].apply(str).str.zfill(2)
-    df.loc[df['HH'] == '24', 'HH'] = '00'
-    df['date'] = df['date'].apply(str)
-    df['date'] = df['date'] + ' ' + df['HH']
-    df = df.drop(columns=['HH'])
-    df['date'] = pd.to_datetime(df['date'], format='%Y%m%d %H')
-    df['sensor'] = 'sensor 1-1'
-    df.to_csv('vorigemaand_clean.csv')
+    return df
+
+def rename_columns(df, column_name_mapper):
+    df = df.rename(columns=column_name_mapper)
+    return df
+
+def filter_columns(df, columns_to_keep):
+    df = df.loc[:, columns_to_keep]
+    return df
+
+def select_rows_by_station(df, station_number):
+    df = df.loc[df["station_number"] == station_number]
+    df = df.drop(columns=["station_number"]).reset_index()
+    return df
+
+
+def fix_temperature_column(df):
+    df["temperature"] = pd.to_numeric(df["temperature"])
+    df["temperature"] = df["temperature"] / 10
+    return df
+
+def fix_hour_column(df):
+    df["hour"] = df["hour"].apply(str).str.zfill(2)
+    df.loc[df["hour"] == "24", "hour"] = "00"
+    return df
+
+def fix_date_column(df):
+    df["date"] = df["date"].apply(str)
+    df["date"] = df["date"] + " " + df["hour"]
+    df["date"] = pd.to_datetime(df["date"], format="%Y%m%d %H")
+    return df
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/raw/vorigemaand.csv")
+
+    column_name_mapper = {"YYYYMMDD": "date", "# STN": "station_number", "HH":'hour', 'T':'temperature'}
+    columns_to_keep = ["date", "hour", "temperature", "station_number"]
+    station_number = 279
+    sensor_name = '1-1'
+    
+    df = (
+        df
+        .pipe(strip_column_names)
+        .pipe(rename_columns, column_name_mapper)
+        .pipe(filter_columns, columns_to_keep)
+        .pipe(select_rows_by_station, station_number)
+        .pipe(fix_temperature_column)
+        .pipe(fix_hour_column)
+        .pipe(fix_date_column)
+    )
+
+    # df = df.set_index('date')
+    df = df.drop(columns=["index"])
+    df = df.drop(columns=["hour"])
+    # df["sensor"] = "sensor 1-1"
+
+    # df["start_day"] = False
+    # mask = df.date.dt.hour == 0
+    # df.loc[mask, "start_day"] = True
+
+    df.to_csv(f"data/vorigemaand_clean_sensor_{sensor_name}.csv")
 
     df
 
+    sensor_data = pd.read_csv(f"data/vorigemaand_clean_sensor_{sensor_name}.csv", index_col=0)
+
+    # sensor_data.index = pd.to_datetime(sensor_data.index, format="%Y%m%d %H")
+    sensor_data
 
     # df = pd.read_csv('vorigemaand_clean.csv', index_col=0)
-    # df.plot(x='date', y='T')
+    # df.plot(x='date', y='temperature')
     # plt.show()
     # print('daan')
